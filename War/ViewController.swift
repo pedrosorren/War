4f9f6f3a0ad18840a2154a528a163cf0cd9e063c
Pedro <pedro@pedrosorren.com>
2015-05-13 19:33:05 -0700

    Adding Erros

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
 
     }
 
@@ -27,6 +27,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+@IBAction func playRoundTapped(sender: UIButton) {
+
+
+    
+    }
 
 }
 
